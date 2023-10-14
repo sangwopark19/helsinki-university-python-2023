c3af531265dee44cc2a4d6da4f28e5dffd960479
sangwopark19 <sangwopark19@gmail.com>
2023-10-14 13:57:56 +0900

    사용자로부터의 정보를 받은 input 함수

--- part01-08_name_and_address/src/name_and_address.py
+++ part01-08_name_and_address/src/name_and_address.py
@@ -1,1 +1,8 @@
 # Write your solution here
+name = input("Given name:")
+familyName = input("Family name:")
+address = input("Street address:")
+zipCode = input("City and postal code:")
+print(name + " " +  familyName)
+print(address)
+print(zipCode)
--- part01-09_utterances/src/utterances.py
+++ part01-09_utterances/src/utterances.py
@@ -1,5 +1,5 @@
 # Fix the code
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-print(part + part + part)
+part1 = input("The 1st part: ")
+part2 = input("The 2st part: ")
+part3 = input("The 3st part: ")
+print(part1 + "-" + part2 + "-" + part3 + "!")
