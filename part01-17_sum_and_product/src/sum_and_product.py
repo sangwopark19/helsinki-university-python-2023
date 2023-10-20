cc7113a814788ccddd26eceb51353953064a207b
sangwopark19 <sangwopark19@gmail.com>
2023-10-20 19:19:34 +0900

    Sum and product

--- part01-17_sum_and_product/src/sum_and_product.py
+++ part01-17_sum_and_product/src/sum_and_product.py
@@ -1,1 +1,7 @@
 # Write your solution here
+num1 = int(input("Number 1:"))
+num2 = int(input("Number 2:"))
+sum = num1 + num2
+multi = num1 * num2
+print(f"The sum of the numbers: {sum}")
+print(f"The product of the numbers: {multi}")
