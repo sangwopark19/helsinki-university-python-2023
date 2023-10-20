a7acb1c9ca0723ecf59512cdd5f684113b45ced8
sangwopark19 <sangwopark19@gmail.com>
2023-10-20 19:10:15 +0900

    코드수정:product / 연산자 약어 활용

--- part01-16_product/src/product.py
+++ part01-16_product/src/product.py
@@ -1,8 +1,8 @@
 # Fix the code
 number = int(input("Please type in the first number: "))
-number = int(input("Please type in the second number: "))
-number = int(input("Please type in the third number: "))
+number *= int(input("Please type in the second number: "))
+number *= int(input("Please type in the third number: "))
 
-product = number * number * number
+product = number
 
 print("The product is", product)
