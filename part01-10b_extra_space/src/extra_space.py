41a073f1be92e6543bc2dbefcf5821b788f2515d
sangwopark19 <sangwopark19@gmail.com>
2023-10-21 16:55:59 +0900

    extra_space // f-string을 사용하는게 좋다

--- part01-10b_extra_space/src/extra_space.py
+++ part01-10b_extra_space/src/extra_space.py
@@ -10,9 +10,11 @@ level3 = "semiprofessional"
 lower = 2000
 upper = 3000
 
-print("my name is ", name, " , I am ", age, "years old")
+print(f"my name is {name}, I am {age} years old")
+print()
 print("my skills are")
-print("- ", skill1, " (", level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )")
-print("I am looking for a job with a salary of", lower, "-", upper, "euros per month")
+print(f" - {skill1} ({level1})")
+print(f" - {skill2} ({level2})")
+print(f" - {skill3} ({level3})")
+print()
+print(f"I am looking for a job with a salary of {lower}-{upper} euros per month")
