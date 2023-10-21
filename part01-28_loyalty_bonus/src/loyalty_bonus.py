58ccf5cd9b0869a5f6449b36b7e722afac6bdad9
sangwopark19 <sangwopark19@gmail.com>
2023-10-21 20:33:13 +0900

    Loyalty bonus / 로열티 보너스 / 조건문

--- part01-28_loyalty_bonus/src/loyalty_bonus.py
+++ part01-28_loyalty_bonus/src/loyalty_bonus.py
@@ -1,11 +1,12 @@
 # Fix the program
 points = int(input("How many points are on your card? "))
+total = points
 if points < 100:
-    points *= 1.1
+    total *= 1.1
     print("Your bonus is 10 %")
 
 if points >= 100:
-    points *= 1.15
+    total *= 1.15
     print("Your bonus is 15 %")
 
-print("You now have", points, "points")
+print("You now have", total, "points")
