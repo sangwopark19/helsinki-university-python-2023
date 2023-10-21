e3a09063e321ad67f02ffea60f2c9e7c3e6b5088
sangwopark19 <sangwopark19@gmail.com>
2023-10-21 20:29:13 +0900

    Daily wages / 일용직 / 조건문

--- part01-27_daily_wages/src/daily_wages.py
+++ part01-27_daily_wages/src/daily_wages.py
@@ -1,1 +1,7 @@
-# Write your solution here
+# Write your solution he
+wage = float(input("Hourly wage:"))
+worked = int(input("Hours worked:"))
+week = input("Day of the week:")
+if week == "Sunday":
+	wage *= 2
+print(f"Daily wages: {wage * worked} euros")
