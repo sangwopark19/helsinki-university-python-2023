31a963036de7f8f8479ef35440c1ecf02853eacb
sangwopark19 <sangwopark19@gmail.com>
2023-10-21 20:59:25 +0900

    Students in groups / 그룹의 학생 / 산술 연산

--- part01-20_students_in_groups/src/students_in_groups.py
+++ part01-20_students_in_groups/src/students_in_groups.py
@@ -1,1 +1,7 @@
-# Write your solution here
+# Write your solution herestudents
+students = int(input("How many students on the course?"))
+size = int(input("Desired group size?"))
+group = students // size
+if students % size > 0:
+	group += 1
+print(f"Number of groups formed: {group}")
