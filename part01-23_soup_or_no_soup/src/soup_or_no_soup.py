71b244b3582558597564adbb606a192901e380cf
sangwopark19 <sangwopark19@gmail.com>
2023-10-21 20:01:55 +0900

    Soup or no soup / 수프 또는 수프 없음

--- part01-23_soup_or_no_soup/src/soup_or_no_soup.py
+++ part01-23_soup_or_no_soup/src/soup_or_no_soup.py
@@ -1,1 +1,7 @@
 # Write your solution here
+SOUP_PRICE = 5.90
+name = input("Please tell me your name:")
+if name != "Jerry":
+	soup = int(input("How many portions of soup?")) * SOUP_PRICE
+	print(f"The total cost is {soup}")
+print("Next please!")
